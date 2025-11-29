30a4fb7f21e566b45238e19653a457280ad123f9
capzlock35 <capzlock35@gmail.com>
2025-11-29 15:00:39 +0800

    A

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './index.css';
+import logo from './assets/logo.png'; // <-- import your logo
+
+// set page title
+document.title = 'AWMA-QA-DEV';
+
+// ensure there's a favicon link and set it to the imported asset
+(() => {
+  const head = document.getElementsByTagName('head')[0];
+  let icon = head.querySelector("link[rel*='icon']");
+  if (!icon) {
+    icon = document.createElement('link');
+    icon.rel = 'icon';
+    head.appendChild(icon);
+  }
+  icon.type = 'image/png';
+  icon.href = logo;
+})();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
